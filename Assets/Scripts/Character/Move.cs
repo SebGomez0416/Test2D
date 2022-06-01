d3efd3244f553f2a1ca4ba2ccaf618e34e8d406d
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-06-01 19:30:53 -0300

    Add Limits

--- Assets/Scripts/Character/Move.cs
+++ Assets/Scripts/Character/Move.cs
@@ -1,30 +1,55 @@
+using System.Drawing;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     [SerializeField] private float speed;
-    
+    private Camera cam;
+    private Vector3 size;
+    private Vector3 max;
+    private Vector3 min;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        cam= Camera.main;
     }
 
     private void FixedUpdate()
     {
         Movement();
+        
     }
     
     private void Movement()
     {
         Vector2 movement;
+        movement.x = Input.GetAxisRaw("Horizontal")*speed* Time.fixedDeltaTime;
+        movement.y= Input.GetAxisRaw("Vertical")*speed* Time.fixedDeltaTime;
 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y= Input.GetAxisRaw("Vertical");
+        rb.position += movement;
+        Limits();
         
-        rb.MovePosition(rb.position + movement*(speed* Time.fixedDeltaTime));
+        movement.x = Mathf.Clamp(rb.position.x,(min.x+size.x),(max.x-size.x));
+        movement.y=  Mathf.Clamp(rb.position.y,(min.y+size.y),(max.y-size.y));
+
+        rb.MovePosition(movement);
     }
 
+    private void Limits()
+    {
+        size = cam.ScreenToWorldPoint(sr.bounds.size);
+        size.x /= size.z;
+        size.y /= size.z;
+        
+        max = cam.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,0));
+        min = cam.ScreenToWorldPoint(Vector3.zero);
+        
+    }
+
+   
    
 }
